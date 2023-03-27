b212bcc2557f98fb41b81faf9cb824682f51e098
grafika10 <grafika10@ucione.local>
2023-03-27 21:45:47 +0200

    added header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,20 +1,27 @@
 import logo from "./../assets/logo.png";
+import hamburger from "./../assets/icon-hamburger.svg";
 
 const Header = () => {
   return (
     <header className="header">
-      <div>
-        <img className="header__logo" src={logo} alt="" />
-      </div>
-      <div className="header__actions">
-        <nav className="header__nav">
-          <a className="header__nav__item" href="">
-            Home
-          </a>
-          <a className="header__nav__item" href="">
-            About
-          </a>
-        </nav>
+      <div className="header__inner">
+        <div>
+          <img className="header__logo" src={logo} alt="" />
+        </div>
+        <div className="header__actions">
+          <nav className="header__nav">
+            <a className="header__nav__item" href="">
+              Home
+            </a>
+            <a className="header__nav__item" href="">
+              About
+            </a>
+          </nav>
+          <div>KR</div>
+        </div>
+        <div className="header__hamburger">
+          <img src={hamburger} alt="hamburger menu icon" />
+        </div>
       </div>
     </header>
   );
