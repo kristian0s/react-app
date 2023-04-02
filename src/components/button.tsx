d045d207e3a3a8a32828e133a9ac9027a2bb87db
Kristian Radoš <kristian0s288@gmail.com>
2023-04-02 15:09:16 +0200

    changed button css

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,5 +1,10 @@
 const Button = () => {
-    return <button className="button">Click me!</button>;
-  };
-  
-  export default Button;
+  return (
+    <div className="btn-container">
+      <h2 className="btn-container__title">Button</h2>
+      <button className="btn-container__btn">Click me!</button>
+    </div>
+  );
+};
+
+export default Button;
--- src/components/checkbox.tsx
+++ src/components/checkbox.tsx
@@ -1,9 +1,10 @@
 const Checkbox = () => {
-    return <div>
-                <span className="checkmark"></span>
-                <input type="checkbox"/>
-                <label className="check_label">Moze?</label>
-            </div>
-  };
-  
-  export default Checkbox;
+  return (
+    <div className="checkbox-container">
+      <label className="lbl">Može?</label>
+      <input type="checkbox" />
+    </div>
+  );
+};
+
+export default Checkbox;
