56f25495bb3d78479ccd8ca9465857cc9ab56b97
Kristian Radoš <kristian0s288@gmail.com>
2023-04-03 17:07:13 +0200

    change to form and checkbox

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,8 +1,37 @@
 const Form = () => {
-    return <div>
-                <label>Write your password: </label>
-                <input type="password" placeholder="Enter your password" />
-            </div>
-  };
-  
-  export default Form;
+  return (
+    <div className="form-cont">
+      <h2 className="form-cont__title">Form</h2>
+      <form>
+        <ul className="form-cont__lst">
+          <li>
+            <label className="form-cont__form-cont__lst__form-cont__lbl">
+              Name:
+            </label>
+            <input
+              className="form-cont__form-cont__lst__form-cont__inpt"
+              type="text"
+            />
+          </li>
+          <li>
+            <label className="form-cont__form-cont__lst__form-cont__lbl">
+              Email:
+            </label>
+            <input
+              className="form-cont__form-cont__lst__form-cont__inpt"
+              type="email"
+            />
+          </li>
+          <li>
+            <label className="form-cont__form-cont__lst__form-cont__lbl">
+              Message:
+            </label>
+            <textarea></textarea>
+          </li>
+        </ul>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,19 +1,21 @@
 import phone from "./../assets/phone.svg";
 
 const Modal = () => {
-    return <div className="modal">
-        <div className="modal__header">
-            <img className="modal__header__img" src={phone} alt="phone image" />
-        </div>
-        <div className="modal__content">
-            <h2 className="modal__content__title">Dolazni poziv!</h2>
-            <p className="modal__content__num">+385123456789</p>
-        </div>
-        <div className="modal__btn">
-            <button className="modal__btn__accept">Prihvati!</button>
-            <button className="modal__btn__decline">Odbij!</button>
-            </div>  
-  </div>
-  };
-  
-  export default Modal;
+  return (
+    <div className="modal">
+      <div className="modal__header">
+        <img className="modal__header__img" src={phone} alt="phone image" />
+      </div>
+      <div className="modal__content">
+        <h2 className="modal__content__title">Dolazni poziv!</h2>
+        <p className="modal__content__num">+385123456789</p>
+      </div>
+      <div className="modal__btn">
+        <button className="modal__btn__accept">Prihvati!</button>
+        <button className="modal__btn__decline">Odbij!</button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
