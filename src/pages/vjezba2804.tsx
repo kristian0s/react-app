674327635e8c69e2de93748ec5e638808674c6e6
Kristian Radoš <kristian0s288@gmail.com>
2023-05-02 19:53:43 +0200

    added page vjezba2804

---
+++ src/pages/vjezba2804.tsx
@@ -0,0 +1,179 @@
+type GenderType = "male" | "female";
+
+type StudentType = {
+  name: string;
+  lastName: string;
+  age: number;
+  gender: GenderType;
+  height: number;
+  hobby: string;
+  employed: boolean;
+  favoriteColor: string;
+};
+
+const Vjezba2804 = () => {
+  const frontendRazred: StudentType[] = [
+    {
+      name: "Ivan",
+      lastName: "Sakoman",
+      age: 29,
+      gender: "male",
+      height: 183,
+      hobby: "Cycling",
+      employed: true,
+      favoriteColor: "red",
+    },
+    {
+      name: "Goran",
+      lastName: "Viljanac",
+      age: 19,
+      gender: "male",
+      height: 185,
+      hobby: "Musician",
+      employed: true,
+      favoriteColor: "green",
+    },
+    {
+      name: "Katharina",
+      lastName: "Zlosa",
+      age: 27,
+      gender: "female",
+      height: 180,
+      hobby: "reading",
+      employed: true,
+      favoriteColor: "green",
+    },
+    {
+      name: "Antun",
+      lastName: "Glumac",
+      age: 22,
+      gender: "male",
+      height: 185,
+      hobby: "Football",
+      employed: false,
+      favoriteColor: "Red",
+    },
+    {
+      name: "Nikolina",
+      lastName: "Kirčanski",
+      age: 40,
+      gender: "female",
+      height: 164,
+      hobby: "Martial arts",
+      employed: true,
+      favoriteColor: "black",
+    },
+    {
+      name: "Robert",
+      lastName: "Đujić",
+      age: 24,
+      gender: "male",
+      height: 182,
+      hobby: "Books, movies, games",
+      employed: false,
+      favoriteColor: "silver",
+    },
+    {
+      name: "Monika",
+      lastName: "Ivankovic",
+      age: 23,
+      gender: "female",
+      height: 164,
+      hobby: "roller skating",
+      employed: true,
+      favoriteColor: "grey",
+    },
+    {
+      name: "Maja",
+      lastName: "Juratovac",
+      age: 32,
+      gender: "female",
+      height: 169,
+      hobby: "dancing",
+      employed: true,
+      favoriteColor: "blue",
+    },
+    {
+      name: "Ivan",
+      lastName: "Puljić",
+      age: 25,
+      gender: "male",
+      height: 175,
+      hobby: "running",
+      employed: false,
+      favoriteColor: "blue",
+    },
+    {
+      name: "Sara",
+      lastName: "Cindric",
+      age: 24,
+      gender: "female",
+      height: 161,
+      hobby: "writing",
+      employed: false,
+      favoriteColor: "black",
+    },
+    {
+      name: "Kristian",
+      lastName: "Radoš",
+      age: 30,
+      gender: "male",
+      height: 188,
+      hobby: "Netflix",
+      employed: true,
+      favoriteColor: "blue",
+    },
+    {
+      name: "Ivana",
+      lastName: "Arbutina",
+      age: 42,
+      gender: "female",
+      height: 166,
+      hobby: "slikanje",
+      employed: false,
+      favoriteColor: "red",
+    },
+  ];
+
+  //1. Zadatak
+  //Pronađi sve osobe kojima ime počinje na slovo "M"
+  //Pronađi sve osobe koje su vislje od 160 a niže od 180
+  //Pronađi sve osobe kojima je najdraža boja "red"
+  //Pronađi sve osobe kojima prezime završava na "c" ili "ć"
+
+  //2. Zadatak
+  //Ispiši u tablicu sve članove razreda
+
+  //3. Zadatak
+  //Obojaj tekst "favoriteColor" u boju koja je napisana npr ako je favoriteColor: red, taj tekst u tablici treba biti crvene boje
+
+  return (
+    <div className="container">
+      <h1>Vjezba 2804</h1>
+      <hr />
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Hobby</th>
+              <th>favoriteColor</th>
+            </tr>
+          </thead>
+          <tbody>
+            {frontendRazred.map((student) => {
+              return (
+                <tr>
+                  <td>{student.name}</td>
+                  <td>{student.hobby}</td>
+                  <td>{student.favoriteColor}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+export default Vjezba2804;
