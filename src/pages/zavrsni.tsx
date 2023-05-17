45def5e82e5fd3a9ba80a7b9e94f22ac2c65be11
Kristian Radoš <kristian0s288@gmail.com>
2023-05-17 18:12:42 +0200

    installed playwright, made changes to zavrsni page

--- src/pages/zavrsni.tsx
+++ src/pages/zavrsni.tsx
@@ -1,8 +1,11 @@
 const Zavrsni = () => {
   return (
-    <div className="containe__chat">
+    <div className="chat">
       <div className="chat__screen"></div>
-      <div className="chat__input"></div>
+      <div className="chat__input">
+        <div className="chat__input__field"></div>
+        <button className="chat__input__btn">Send</button>
+      </div>
     </div>
   );
 };
